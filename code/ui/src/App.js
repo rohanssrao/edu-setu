07dd155c8410dc8945ad2d4d15a3ca5867ce670f
Dhrumil Shah <dhrumilshah0310@gmail.com>
2022-09-18 12:59:02 -0400

    Fixed conflicts

--- code/ui/src/App.js
+++ code/ui/src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import "./App.css";
 import styled from "styled-components";
 import { AccountBox } from "./components/accountBox";
@@ -47,4 +20,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 8e330b3910137c8f3285740c9b6c73bb6004f99b
--- code/ui/src/index.js
+++ code/ui/src/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom/client';
-=======
 import ReactDOM from 'react-dom';
->>>>>>> 8e330b3910137c8f3285740c9b6c73bb6004f99b
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
->>>>>>> 8e330b3910137c8f3285740c9b6c73bb6004f99b
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
 reportWebVitals();
-=======
-reportWebVitals();
->>>>>>> 8e330b3910137c8f3285740c9b6c73bb6004f99b
