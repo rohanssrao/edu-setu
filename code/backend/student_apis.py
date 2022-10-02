7a177efde1c8220ad4cdfa140deb91f17228718e
Yashasya <yashasya.shah@gmail.com>
2022-10-02 11:45:50 -0400

    Created API for displaying applications

--- code/backend/student_apis.py
+++ code/backend/student_apis.py
@@ -29,3 +29,32 @@ def get_all_users():
             con.close()
         except:
             pass
+
+
+def get_all_application():
+    con = connect()
+    if not con:
+        return prepare_response(False,  "Unable to connect to database.")
+    try:
+        curs = con.cursor()
+    except Exception as e:
+        print(e)
+        return prepare_response(False,  "Unable to connect to database.")
+    try:
+        query = '''SELECT * FROM APPLICATIONS'''
+        curs.execute(query)
+        curs.rowfactory = makeDictFactory(curs)
+        response = curs.fetchall()
+        try:
+            con.close()
+        except:
+            pass
+        return prepare_response(True, response)
+    except Exception as e:
+        print(e)
+        return {"status": False, "data": str(e)}
+    finally:
+        try:
+            con.close()
+        except:
+            pass
