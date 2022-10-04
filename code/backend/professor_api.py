6b1fcb46a980ccd72bea90e85624b85fa54ab8a7
Harshil Sanghavi <harshilsan7@gmail.com>
2022-10-04 11:51:19 -0400

    DEBUG

--- code/backend/professor_api.py
+++ code/backend/professor_api.py
@@ -32,7 +32,35 @@ def add_posting(data):
     finally:
         disconnect(con)
         
-    
+        
+def get_all_postings():
+    con = connect()
+    if not con:
+        return prepare_response(False,  "Unable to connect to database.")
+    try:
+        curs = con.cursor()
+    except Exception as e:
+        print(e)
+        return prepare_response(False,  "Unable to connect to database.")
+    try:
+        query = '''SELECT * FROM USERS'''
+        curs.execute(query)
+        curs.rowfactory = makeDictFactory(curs)
+        response = curs.fetchall()
+        try:
+            con.close()
+        except:
+            pass
+        return prepare_response(True, response)
+    except Exception as e:
+        print(e)
+        return {"status": False, "data": str(e)}
+    finally:
+        try:
+            con.close()
+        except:
+            pass
+   
 
 def get_all_postings_by_professor(data):
     con = connect()
@@ -64,36 +92,6 @@ def get_all_postings_by_professor(data):
         except:
             pass
 
-
-
-
-def get_all_postings():
-    con = connect()
-    if not con:
-        return prepare_response(False,  "Unable to connect to database.")
-    try:
-        curs = con.cursor()
-    except Exception as e:
-        print(e)
-        return prepare_response(False,  "Unable to connect to database.")
-    try:
-        query = '''SELECT * FROM POSTINGS'''
-        curs.execute(query)
-        curs.rowfactory = makeDictFactory(curs)
-        response = curs.fetchall()
-        try:
-            con.close()
-        except:
-            pass
-        return prepare_response(True, response)
-    except Exception as e:
-        print(e)
-        return {"status": False, "data": str(e)}
-    finally:
-        try:
-            con.close()
-        except:
-            pass
 
 
 def update_posting(data):
