fe9132c798852729305ef0215646692b7ddb47b7
Harshil Sanghavi <harshilsan7@gmail.com>
2022-10-04 00:05:50 -0400

    date debug

--- code/backend/professor_api.py
+++ code/backend/professor_api.py
@@ -1,7 +1,7 @@
 from os import posix_fallocate
 from utils import *
 import bcrypt
-import datetime
+from datetime import date
 
 def add_posting(data):
     try:
@@ -15,9 +15,9 @@ def add_posting(data):
         description = data["notes"]
         location = data["area"]
         prerequisites = data["prereq"]
-        current_date = datetime.datetime.now()
-        created_at = "2022-10-04 "
-        updated_at = "2022-10-04 "
+        current_date = date.today()
+        created_at = current_date
+        updated_at = current_date
         # Insert application into database
         cur = con.cursor()
         query = "INSERT INTO POSTINGS ( TITLE, PROFESSOR, DESCRIPTION, LOCATION, PREREQUISITES, CREATED_AT, UPDATED_AT ) VALUES (:1,:2,:3,:4,:5,:6,:7)"
