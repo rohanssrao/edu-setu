9addd388bc7e82cb66ce190e5c9abadc8fe429cb
anishasc99 <anishasc99@gmail.com>
2022-10-04 10:44:59 -0400

    changed studentDashboard to class

--- code/ui/src/components/studentdashboard/studentdashboard.jsx
+++ code/ui/src/components/studentdashboard/studentdashboard.jsx
@@ -8,6 +8,7 @@ import Button from 'react-bootstrap/Button';
 import FormControl from 'react-bootstrap/FormControl';
 import InputGroup from 'react-bootstrap/InputGroup';
 import NavBar from "../navbar";
+import users from './user.json'
 
 function MyVerticallyCenteredModal(props) {
 
@@ -40,114 +41,135 @@ function MyVerticallyCenteredModal(props) {
   );
 }
 
-function apply(jobs) {
-  console.log(jobs);
-}
-function saveJob(jobs) {
-  console.log(jobs);
-}
-export function StudentDashboard() {
-  const [modalShow, setModalShow] = React.useState(false);
-  const [currentJob, setCurrentJob] = React.useState({ "role": "", "prerequisites": "", "description": "" });
-  return (
-    <>
-      <NavBar />
-      <h1 className="display-3">Search for a research role</h1>
-      <p>Enhance your skills by working as a research assistant under professors</p>
-      <div className="container">
-        <div className="row justify-content-center align-items-center">
-          <InputGroup className="col-sm text-center">
-            <FormControl
-              placeholder="What are you looking for?"
-              aria-label="Search"
-              aria-describedby="basic-addon2"
-            />
-            <Button variant="outline-secondary" id="button-addon2">
-              Search
-            </Button>
-          </InputGroup>
-          <Dropdown className="col-sm text-center">
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-              Department
-            </Dropdown.Toggle>
 
-            <Dropdown.Menu variant="dark">
+export class StudentDashboard extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      modalShow:false,
+      currentJob:{}
+
+    }
+  }
+  componentWillMount() {
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].user_id == this.state.user_id) {
+        this.setState({ current_user: users[i] }, () => {
+          console.log(this.state.current_user);
+        });
+      }
+    }
+  }
+  apply(jobs) {
+    console.log(jobs);
+  }
+  saveJob(jobs) {
+    console.log(jobs);
+  }
+  //const[modalShow, setModalShow] = React.useState(false);
+  //const[currentJob, setCurrentJob] = React.useState({ "role": "", "prerequisites": "", "description": "" });
+  render() {
+    return (
+      <>
+        <NavBar />
+        <h1 className="display-3">Search for a research role</h1>
+        <p>Enhance your skills by working as a research assistant under professors</p>
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <InputGroup className="col-sm text-center">
+              <FormControl
+                placeholder="What are you looking for?"
+                aria-label="Search"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="outline-secondary" id="button-addon2">
+                Search
+              </Button>
+            </InputGroup>
+            <Dropdown className="col-sm text-center">
+              <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                Department
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu variant="dark">
+                {
+                  jobs.map(jobs => (
+                    <Dropdown.Item href="#/action-1" >{jobs.department}</Dropdown.Item>
+                  )
+                  )
+                }
+              </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown className="col-sm">
+              <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                Type
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu variant="dark">
+                <Dropdown.Item href="#/action-1">Remote</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Hybrid</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Office</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+        <link rel="stylesheet" href="studentDashboard.css"></link>
+
+        <div className="container">
+          <Table striped hover className="col-6">
+            <thead>
+              <tr>
+                <th>Job ID</th>
+                <th >Role</th>
+                <th>Professor</th>
+                <th>Department</th>
+                <th>Type</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
               {
                 jobs.map(jobs => (
-                  <Dropdown.Item href="#/action-1" >{jobs.department}</Dropdown.Item>
-                )
-                )
+                  <tr>
+                    <td>{jobs.id}</td>
+                    <td><a className="link-primary" onClick={() => {
+                      this.setState({modalShow:true});
+                      this.setState({currentJob: jobs})
+                      //setCurrentJob(job => ({ ...job, role: jobs.role, description: jobs.description, prerequisites: jobs.prerequisites }));
+                    }}>
+                      {jobs.role}
+                    </a>
+
+                      <MyVerticallyCenteredModal
+                        show={this.state.modalShow} currentJob={this.state.currentJob}
+                        onHide={() => this.setState({modalShow:false})}
+                      /></td>
+                    <td>{jobs.professor}</td>
+                    <td>{jobs.department}</td>
+                    <td>{jobs.type}</td>
+                    <td>
+                      <Dropdown>
+                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                          Actions
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu variant="dark">
+                          <Dropdown.Item href="#/action-1" active onClick={(e) => this.apply(jobs, e)}>Apply</Dropdown.Item>
+                          <Dropdown.Item href="#/action-2">Save for Later</Dropdown.Item>
+                          <Dropdown.Item href="#/action-3">Get shareable URL</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </td>
+                  </tr>
+
+                ))
               }
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown className="col-sm">
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-              Type
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu variant="dark">
-              <Dropdown.Item href="#/action-1">Remote</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Hybrid</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Office</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+            </tbody>
+          </Table>
         </div>
-      </div>
-      <link rel="stylesheet" href="studentDashboard.css"></link>
-
-      <div className="container">
-        <Table striped hover className="col-6">
-          <thead>
-            <tr>
-              <th>Job ID</th>
-              <th >Role</th>
-              <th>Professor</th>
-              <th>Department</th>
-              <th>Type</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              jobs.map(jobs => (
-                <tr>
-                  <td>{jobs.id}</td>
-                  <td><a className="link-primary" onClick={() => {
-                    setModalShow(true);
-                    setCurrentJob(job => ({ ...job, role: jobs.role, description: jobs.description, prerequisites: jobs.prerequisites }));
-                  }}>
-                    {jobs.role}
-                  </a>
-
-                    <MyVerticallyCenteredModal
-                      show={modalShow} currentJob={currentJob}
-                      onHide={() => setModalShow(false)}
-                    /></td>
-                  <td>{jobs.professor}</td>
-                  <td>{jobs.department}</td>
-                  <td>{jobs.type}</td>
-                  <td>
-                    <Dropdown>
-                      <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                        Actions
-                      </Dropdown.Toggle>
-
-                      <Dropdown.Menu variant="dark">
-                        <Dropdown.Item href="#/action-1" active onClick={(e) => apply(jobs, e)}>Apply</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Save for Later</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Get shareable URL</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </td>
-                </tr>
-
-              ))
-            }
-          </tbody>
-        </Table>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 
 }
 
