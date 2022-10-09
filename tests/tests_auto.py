8dd44009e6950dc351e18404793f6977ab27978b
Jayraj Mulani <jayrajm.mulani@gmail.com>
2022-10-09 19:18:34 -0400

    Update tests_auto.py

---
+++ tests/tests_auto.py
@@ -0,0 +1,19 @@
+from main import app
+import auth
+import student_apis
+import utils
+import string
+import sys
+import traceback
+from flask import Flask
+import json
+import pytest
+
+sys.path.append("../code/backend")
+
+base_url = "http://140.238.250.0:5000"
+
+
+def test_index_route():
+    response = app.test_client().get(f'{base_url}/')
+    assert response.status_code == 200
